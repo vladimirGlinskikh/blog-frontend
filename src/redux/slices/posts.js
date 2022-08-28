dee163522787e261119062196406642bf88a12db
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-08-28 16:40:03 +0300

    Save the status of the request and the articles themselves to the state

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -20,7 +20,21 @@ const initialState = {
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducer: {},
+    reducers: {},
+    extraReducers: {
+        [fetchPosts.pending]: (state) => {
+            state.posts.items = [];
+            state.posts.status = 'loading';
+        },
+        [fetchPosts.fulfilled]: (state, action) => {
+            state.posts.items = action.payload;
+            state.posts.status = 'loaded';
+        },
+        [fetchPosts.rejected]: (state) => {
+            state.posts.items = [];
+            state.posts.status = 'error';
+        },
+    },
 });
 
 export const postsReducer = postsSlice.reducer;
